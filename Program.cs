cdb5ce00e5920b0aac5fa4b901af2b38e649bdcf
Andrew Dunne <drewdunne88@gmail.com>
2019-11-06 21:04:11 -0800

    Made the code worse

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello motherfucka");
+            Console.WriteLine("Hello Bitches!");
+            // Restored it to bitches for you so you don't feel bad ;) <3 <3
         }
     }
 }
